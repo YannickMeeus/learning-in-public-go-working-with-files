58bf9fc1ecd80557a829521ab9b861ec876b1ed4
Yannick Meeus <yannick.meeus@gmail.com>
2022-06-27 11:28:08 +0300

    Make things not break all the time

--- main.go
+++ main.go
@@ -9,9 +9,10 @@ func main() {
 	const directory = "assets"
 	log.Print("1. Create a directory named \"assets\" if it does not exist")
 
-	err := os.Mkdir(directory, os.ModePerm)
+	err := os.MkdirAll(directory, os.ModePerm)
 
 	if err != nil {
 		log.Fatal(err)
 	}
+	log.Print("1. Directory named \"assets\" created")
 }
